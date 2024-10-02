e1465804e2d01011b73df6e5dad754f572226229
sandy0903 <44163875+sandy0903@users.noreply.github.com>
2024-10-02 20:37:57 +0700

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,7 @@
+Step 1: Enter the amount of money
+Step 2: Enter number of time (month,year)
+
+Depends on how long and how much you have , the rate interest equivalent to that
+ 
+=> Result after calculation
+Show the total of the amount after the time you entered
